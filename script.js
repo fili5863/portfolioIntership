b253ebf5ea06fc1493c59888183cdda33b80327f
Jachob Westh <jachobwesth@gmail.com>
2023-03-04 09:43:56 +0100

    convert

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-/* Here the y pos is found and the transition is set to complete when scrolled down 120 px */
+"use strict";
+const splash = document.getElementById("splash");
+const splashHeight = `${splash.offsetHeight}`;
 
-$(window).on("scroll", function () {
-  var yPos = $(this).scrollTop();
-  var yPer = yPos / 120;
+console.log(splashHeight);
 
-  /* The if statement makes sure the percentage doesn't exceed 100% */
-  if (yPer > 1) {
-    yPer = 1;
+document.querySelector("main").onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.querySelector("main").scrollTop > splashHeight / 2) {
+    console.log("hey");
+    document.getElementById("splash").classList.add("scroll");
+  } else {
+    console.log("ho");
   }
-});
-
-/* Store heights in variables */
-var header = $("header");
-var headerHeight = header.height();
+}
